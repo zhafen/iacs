d94726904c8f231905e5da47402985cc0922c18b
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2026-02-16 22:30:27 -0600

    Narrowed explore_hamilton down to a more-accurate use case.

--- iacs/transforms/entity_to_component.py
+++ iacs/transforms/entity_to_component.py
@@ -1,1 +1,19 @@
 """Hamilton DAG for converting entity-centered data to component-centered data."""
+
+import ibis
+
+def raw_entity_first_dict(input_dir: str) -> dict:
+
+    return
+
+def component_first_dict(raw_entity_first_dict: dict) -> dict:
+
+    return
+
+def component_schemas(component_first_dict: dict) -> dict:
+
+    return
+
+def component_database(component_first_dict: dict) -> ibis.BaseBackend:
+
+    return
