458714f5a271ae2f1c7ad47393fece87ca3bb876
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2026-02-16 23:02:09 -0600

    Added a little more to entity_to_component.py

--- iacs/transforms/entity_to_component.py
+++ iacs/transforms/entity_to_component.py
@@ -1,19 +1,33 @@
 """Hamilton DAG for converting entity-centered data to component-centered data."""
 
+from hamilton.function_modifiers import extract_fields
 import ibis
+import pydantic
+
 
 def raw_entity_first_dict(input_dir: str) -> dict:
 
     return
 
+@extract_fields({
+    "schema": dict,
+})
 def component_first_dict(raw_entity_first_dict: dict) -> dict:
 
     return
 
-def component_schemas(component_first_dict: dict) -> dict:
+def pydantic_models(schema: dict) -> dict[pydantic.BaseModel]:
 
     return
 
-def component_database(component_first_dict: dict) -> ibis.BaseBackend:
+
+def raw_component_database(component_first_dict: dict) -> ibis.BaseBackend:
+
+    return
+
+
+def component_database(
+    raw_component_database: ibis.BaseBackend, pydantic_models: dict
+) -> ibis.BaseBackend:
 
     return
