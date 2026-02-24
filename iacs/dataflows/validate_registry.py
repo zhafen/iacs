e41809c3f19c05959697bd66be14dcaeae93bbab
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2026-02-23 22:48:45 -0600

    Sketched out the dataflow for validating the registry.

--- iacs/dataflows/validate_registry.py
+++ iacs/dataflows/validate_registry.py
@@ -16,8 +16,9 @@ def components(registry: Registry) -> dict:
     return registry._components
 
 
-def parent_from_path(spine: ir.Table) -> ir.Table:
-    """Convert the paths in the spine into parent-child relationships.
+def updated_parent(spine: ir.Table, parent: ir.Table) -> ir.Table:
+    """Convert the paths in the spine into parent-child relationships and
+    add them to the parent component.
 
     Parameters
     ----------
@@ -31,11 +32,99 @@ def parent_from_path(spine: ir.Table) -> ir.Table:
     return
 
 
-def updated_parent(parent: ir.Table, parent_from_path: ir.Table) -> ir.Table:
+def validated_field(field: ir.Table) -> ir.Table:
+    """The ((field)) component contains the data for the schema for all components.
+    This includes the ((field)) component itself. We will use the ((field)) component
+    to validate the data in all components, but first we need to use the appropriate
+    records in the ((field)) component to validate the data in just the ((field))
+    component.
+
+    Parameters
+    ----------
+    field : ir.Table
+        _description_
+
+    Returns
+    -------
+    ir.Table
+        _description_
+    """
 
     return
 
 
-def updated_registry(updated_parent: ir.Table) -> Registry:
+def derived_field(validated_field: ir.Table, updated_parent: ir.Table) -> ir.Table:
+    """Component definitions inherit fields from their parents, but can override them.
+    The derived_field table contains the results of applying this inheritance.
+
+    Parameters
+    ----------
+    validated_field : ir.Table
+        _description_
+    updated_parent : ir.Table
+        _description_
+
+    Returns
+    -------
+    ir.Table
+        _description_
+    """
+
+    return
+
+
+def updated_components(
+    updated_parent: ir.Table, derived_field: ir.Table, registry: Registry
+) -> dict:
+    """Store the components updated so far back in the registry.
+
+    Parameters
+    ----------
+    updated_parent : ir.Table
+        _description_
+    derived_field : ir.Table
+        _description_
+
+    Returns
+    -------
+    Registry
+        _description_
+    """
+
+    return
+
+def validated_components(updated_components: dict, derived_field: ir.Table) -> dict:
+    """Use the schemas defined by the ((field)) component to validate and coerce the data in each component.
+
+    Parameters
+    ----------
+    updated_components : dict
+        _description_
+    derived_field : _type_
+        _description_
+
+    Returns
+    -------
+    dict
+        _description_
+    """
+
+    return
+
+def validated_registry(validated_components: dict, registry: Registry) -> Registry:
+    """Store the components back in the registry.
+
+    Parameters
+    ----------
+    validated_components : dict
+        _description_
+    registry : Registry
+        _description_
+
+    Returns
+    -------
+    Registry
+        _description_
+    """
 
     return
