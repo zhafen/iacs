7e811c4f28263964b3fca5b104fb3c7139e6555c
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2026-03-02 18:54:09 -0600

    Added some stubs to derive_components for the future.

--- iacs/dataflows/derive_components.py
+++ iacs/dataflows/derive_components.py
@@ -1,6 +1,10 @@
 """A dataflow for deriving additional components from the base input.
 This is intended to be completed post-validation, so fields need to be derived
 separately as part of validation.
+
+
+Right now this file is being used to track transforms that need to be applied
+to any arbitrary component for the component to be valid.
 """
 
 from hamilton.function_modifiers import extract_fields, unpack_fields
@@ -9,8 +13,57 @@ import ibis.expr.types as ir
 
 from iacs.registry import Registry
 
-@extract_fields({
-    "spine": ir.Table,
-})
-def components(registry: Registry) -> dict:
-    return registry._components
+
+def global_paths(paths: ir.Column, spine: ir.Table) -> ir.Column:
+    """Check for relative paths and convert them to global paths.
+    This is done 
+
+    Parameters
+    ----------
+    paths : ir.Column
+        _description_
+    spine : ir.Table
+        _description_
+
+    Returns
+    -------
+    ir.Column
+        _description_
+    """
+    return
+
+
+def resolved_paths(paths: ir.Column, spine: ir.Table) -> ir.Column:
+    """Convert a column of paths into a column of entity_ids.
+
+    Parameters
+    ----------
+    paths : ir.Column
+        _description_
+
+    Returns
+    -------
+    ir.Column
+        _description_
+    """
+    return
+
+
+def modified_component(component: ir.Table, spine: ir.Table) -> ir.Table:
+    """Apply the modifiers listed in spine to the component.
+    Possible modifiers as of writing:
+    - of: Switches target and source.
+
+    Parameters
+    ----------
+    component : ir.Table
+        _description_
+    spine : ir.Table
+        _description_
+
+    Returns
+    -------
+    ir.Table
+        _description_
+    """
+    return
