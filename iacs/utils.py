1b919b22ab8b4ab5f68d8634bc14d5a73843540c
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2026-03-21 17:46:23 -0500

    Added a get_id util

--- iacs/utils.py
+++ iacs/utils.py
@@ -3,3 +3,8 @@ import hashlib
 def dhash(path: str) -> str:
     """Return a deterministic 12-char hex hash."""
     return hashlib.sha256(path.encode()).hexdigest()[:12]
+
+def get_id(filepath: str, path: str) -> str:
+    """Get the ID from the filepath and path within the file."""
+
+    return dhash(f"{filepath}:{path}")
