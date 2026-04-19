0e3d2982a9cade38fe6f20cfac259e39d49a0d00
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2026-04-19 14:52:20 -0500

    Started building out the fast api app so I could understand it.

--- app.py
+++ app.py
@@ -1,7 +1,57 @@
+import atexit
 from pathlib import Path
 
+from pydantic import BaseModel
 from fastapi import FastAPI
 
 from iacs.architect import Architect
 
 ROOT_DIR = Path(__file__).parent
+COMPONENTS_DIR = ROOT_DIR / "builtins"
+
+
+class Item(BaseModel):
+    name: str
+    description: str | None = None
+    price: float
+    tax: float | None = None
+
+
+class ComponentViewArgs(BaseModel):
+    component_type: str | list[str]
+
+
+app = FastAPI()
+
+a: Architect = Architect.from_manifest(COMPONENTS_DIR)
+
+# We really want to ensure the registry closes out at exit
+atexit.register(a.registry.close)
+
+
+@app.get("/")
+def root():
+    return {"message": f"Architect for {COMPONENTS_DIR}"}
+
+
+@app.get("/components/{component_name}")
+def get_component(component_name: str):
+
+    return {"output": a.get(component_name).__str__()}
+
+
+# @app.get("/view/{component_type}")
+# def view(component_type: str = None):
+# 
+#     return a.view(component_type)
+
+@app.post("/view/{component_type}")
+def view(component_type: str, component_view_args: ComponentViewArgs):
+
+    if component_type is None:
+        component_type = component_view_args.component_type
+
+    return {
+        "component_type": component_type,
+        "view": str(a.view(component_type))
+    }
