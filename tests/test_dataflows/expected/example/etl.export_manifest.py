b014890c2aea0af34be9799872f0e7f55e02f743
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2026-05-17 19:53:30 -0500

    Updated expected with a variable that should fail.

--- tests/test_dataflows/expected/example/etl.export_manifest.py
+++ tests/test_dataflows/expected/example/etl.export_manifest.py
@@ -1,12 +1,13 @@
 import yaml
 
-_file_str = """make_cats_happy:
+_flat_file_str = (
+"""
+make_cats_happy:
     - description:
         value: The mission of our cat-happiness device.
     - requirement:
         value: functional
         priority: 1
-
 feed_and_water_cats:
     - description:
         value: Obviously.
@@ -14,7 +15,28 @@ feed_and_water_cats:
         value: functional
         priority: 1
 """
-
+)
+_hierarchical_file_str = (
+"""
+make_cats_happy:
+    data:
+        - description:
+            value: The mission of our cat-happiness device.
+        - requirement:
+            value: functional
+            priority: 1
+    feed_and_water_cats:
+        - description:
+            value: Obviously.
+        - requirement:
+            value: functional
+            priority: 1
+"""
+)
 entity_first_data = {
-    "examples/example/manifest.yaml": yaml.safe_load(_file_str)
+    "examples/example/manifest.yaml": yaml.safe_load(_flat_file_str)
+}
+# This should NOT work.
+incorrect_entity_first_data = {
+    "examples/example/manifest.yaml": yaml.safe_load(_hierarchical_file_str)
 }
