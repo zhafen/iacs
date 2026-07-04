b3413dcc21987488259f4f176b3945676423b4ed
Zach Hafen-Saavedra <z.hafen.saavedra@gmail.com>
2026-07-03 21:23:38 -0600

    Finished building out a resolved_impact_cost example.

--- tests/test_dataflows/expected/impact-cost_analysis/derive.impact_cost.py
+++ tests/test_dataflows/expected/impact-cost_analysis/derive.impact_cost.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 from iacs.utils import dhash
 
-filepath = "examples/impact-cost/manifest.yaml"
+filepath = "examples/impact-cost_analysis/manifest.yaml"
 
 
 def get_id(path):
@@ -10,7 +10,11 @@ def get_id(path):
 
 resolved_impact_cost = pd.DataFrame([
     {
-        "entity_id": get_id("activities.side_projects.work_on_iacs"),
-        "impact": (0.25 * 4) + (0.5 * 0.2) + (0.25 * 1) + (1 * 1.5),
+        "entity_id": get_id("activities.video_games.expedition_33"),
+        "impact": (impact0 := (0.75 * 4) + (0.25 * 0.2)),
+        # 1 unit of concentration, 40 usd
+        "cost": (cost0 := (1 * 2) + (40 * (4 / 50))),
+        "diff": impact0 - cost0,
+        "ratio": impact0 / cost0,
     },
 ])
